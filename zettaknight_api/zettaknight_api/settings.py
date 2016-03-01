8e549df97f9884e6379abd6e0bc39ca34b95212d
Ralph M Goodberlet <rgoodbe@g.clemson.edu>
2016-03-01 15:33:11 -0500

    Update settings.py

--- zettaknight_api/zettaknight_api/settings.py
+++ zettaknight_api/zettaknight_api/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/opt/clemson/zfs_scripts/zettaknight/zettaknight_api/sqlite3.db',                      # Or path to database file if using sqlite3.
+        'NAME': '../zettaknight/zettaknight_api/sqlite3.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -23,7 +23,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['home.zfs.clemson.edu', 'len014.zfs.clemson.edu']
+ALLOWED_HOSTS = []
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
