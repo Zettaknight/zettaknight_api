bd581e52c52330f811ae46841861178d8ab595d4
Ralph M Goodberlet <rgoodbe@g.clemson.edu>
2016-03-01 15:36:29 -0500

    Delete runserver.sh

--- zettaknight_api/runserver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-date_time=$(date)
-
-echo -e  "\nZettaknight API server started at ${date_time}!\n"
-python /opt/clemson/zfs_scripts/zettaknight/zettaknight_api/zettaknight_api/manage.py runserver 0.0.0.0:8000
-
